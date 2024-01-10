f8980f1c3c6526c85199ccbe198d0bc03f45173c
Rafał Galik <rafal.galik@sadapay.pk>
2024-01-10 13:08:51 +0100

    Checkpoint-Restore bug

--- checkpoint.sh
+++ checkpoint.sh
@@ -16,6 +16,6 @@ echo "Using CRaC enabled JDK $url"
 docker build -t sdeleuze/spring-boot-crac-demo:builder --build-arg CRAC_JDK_URL=$url .
 docker run -d --privileged --rm --name=spring-boot-crac-demo --ulimit nofile=1024 -p 8080:8080 -v $(pwd)/build:/opt/mnt -e FLAG=$1 sdeleuze/spring-boot-crac-demo:builder
 echo "Please wait during creating the checkpoint..."
-sleep 60
+sleep 120
 docker commit --change='ENTRYPOINT ["/opt/app/entrypoint.sh"]' $(docker ps -qf "name=spring-boot-crac-demo") sdeleuze/spring-boot-crac-demo:checkpoint
 docker kill $(docker ps -qf "name=spring-boot-crac-demo")
